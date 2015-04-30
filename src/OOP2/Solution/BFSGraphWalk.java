bacac3bc4475295bae0ca6991e6c70afc425776e
Daniel Mittelman <mittelman@campus.technion.ac.il>
2015-04-30 10:06:48 +0300

    Added BFSGraphWalk and its corresponding test

--- src/OOP2/Solution/BFSGraphWalk.java
+++ src/OOP2/Solution/BFSGraphWalk.java
@@ -1,21 +1,70 @@
 package OOP2.Solution;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
+import OOP2.Provided.Edge;
+import OOP2.Provided.QueueEmptyException;
 import OOP2.Provided.Vertex;
 
-public class BFSGraphWalk implements Iterable<Vertex>{
+public class BFSGraphWalk implements Iterable<Vertex> {
 	
-	/*
-	 * you need to implement Iterable<Vertex>.
-	 * you can inherit other classes or implement other interfaces in addition to Iterable<Vertex>. 
-	 */
-	
-	
-	/*
-	 * you must implement a constructor that takes an initial vertex for the iteration.
-	 */
+	private Vertex mOrigin;
+
 	public BFSGraphWalk(Vertex origin) {
-		
+		mOrigin = origin;
 	}
 
+	@Override
+	public Iterator<Vertex> iterator() {
+		return new Iterator<Vertex>() {
+			PriorityQueueImpl openQueue = new PriorityQueueImpl();
+			List<Vertex> closedList = new ArrayList<Vertex>();
+			Vertex currentVertex;
+			
+			@Override
+			public boolean hasNext() {
+				// If this is the first call to the iterator, there is a next
+				if(closedList.isEmpty() && openQueue.isEmpty() && currentVertex == null) {
+					return true;
+				}
+				
+				// If both the list and queue are empty, and this is not the first iteration,
+				// the algorithm has ended
+				if(!(closedList.isEmpty()) && openQueue.isEmpty()) {
+					return false;
+				}
+				
+				return true;
+			}
+
+			@Override
+			public Vertex next() {
+				if(currentVertex == null) {
+					currentVertex = mOrigin;
+					openQueue.enqueue(currentVertex, currentVertex.Value());
+				}
+				
+				// Dequeue openQueue and set as currentVertex
+				try {
+					currentVertex = openQueue.dequeue();
+				} catch (QueueEmptyException e1) {
+					// This would never happen and does not need to be handled
+				}
+				
+				// Add all the adjacent vertices of the current vertex to the openQueue
+				for(Edge e : currentVertex.getSuccessors()) {
+					Vertex v = e.getEndpoint();
+					if(!(openQueue.contains(v) || closedList.contains(v))) {
+						openQueue.enqueue(v, v.Value());
+					}
+				}
+				
+				// Set the current vertex as closed and return it
+				closedList.add(currentVertex);
+				return currentVertex;
+			}
+		};
+	}
 }
---
+++ src/OOP2/Tests/GraphWalkTests.java
@@ -0,0 +1,68 @@
+package OOP2.Tests;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import OOP2.Provided.Vertex;
+import OOP2.Solution.BFSGraphWalk;
+import OOP2.Solution.VertexImpl;
+
+public class GraphWalkTests {
+	
+	@Test
+	public void BFSGraphWalkTest() {
+		// Build a test graph
+		Vertex v1 = new VertexImpl("A", 1);
+		Vertex v2 = new VertexImpl("B", 2);
+		Vertex v3 = new VertexImpl("C", 3);
+		Vertex v4 = new VertexImpl("D", 4);
+		Vertex v5 = new VertexImpl("E", 5);
+		Vertex v6 = new VertexImpl("F", 6);
+		v1.connect(v2);
+		v2.connect(v4);
+		v4.connect(v1);
+		v4.connect(v5);
+		v4.connect(v6);
+		v5.connect(v4);
+		v6.connect(v3);
+		
+		BFSGraphWalk graphWalk = new BFSGraphWalk(v1);
+		
+		// First check that the iterator runs without problems
+		int counter = 0;
+		for(Vertex v : graphWalk) {
+			Assert.assertNotNull(v);
+			counter++;
+		}
+		Assert.assertEquals(6, counter);
+		
+		// Now check that the iteration is in the correct order
+		Queue<Vertex> correctOrder = new LinkedList<Vertex>(Arrays.asList(v1, v2, v4, v5 ,v6, v3));
+		for(Vertex v : graphWalk) {
+			Assert.assertEquals(correctOrder.poll(), v);
+		}
+		Assert.assertEquals(0, correctOrder.size());
+		
+		// Test again, beginning at v5
+		graphWalk = new BFSGraphWalk(v5);
+		correctOrder = new LinkedList<Vertex>(Arrays.asList(v5, v4, v1, v2, v6, v3));
+		for(Vertex v : graphWalk) {
+			Assert.assertEquals(correctOrder.poll(), v);
+		}
+		Assert.assertEquals(0, correctOrder.size());
+		
+		// Test again, beginning at v3
+		graphWalk = new BFSGraphWalk(v3);
+		correctOrder = new LinkedList<Vertex>(Arrays.asList(v3));
+		for(Vertex v : graphWalk) {
+			Assert.assertEquals(correctOrder.poll(), v);
+		}
+		Assert.assertEquals(0, correctOrder.size());
+		
+	}
+	
+}
